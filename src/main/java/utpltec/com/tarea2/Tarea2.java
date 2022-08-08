7e89576dbf890b2309acb6ddefc3d54a4b9b385c
bryan <bryan@DESKTOP-33PP7LI>
2022-08-07 23:29:48 -0500

    Tarea 223

---
+++ src/main/java/utpltec/com/tarea2/Tarea2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package utpltec.com.tarea2;
+
+/**
+ *
+ * @author bryan
+ */
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
